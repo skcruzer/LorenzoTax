f0717ad0c916dcc4185f39f915e061b919392185
Sean Cruz <seankennethcruz@gmail.com>
2024-07-17 00:57:39 -0700

    cta, footer

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,12 @@
-function CopyMe(text) {
-  navigator.clipboard.writeText(text)
-    .then(() => {
-      console.log(`Copied to clipboard: ${text}`);
-      alert(`Copied to clipboard: ${text}`);
-    })
-    .catch((error) => {
-      console.error(`Could not copy email address: ${error}`);
-    });
-}
+
+// ===================== Copy Email Option =====================
+// function CopyMe(text) {
+//   navigator.clipboard.writeText(text)
+//     .then(() => {
+//       console.log(`Copied to clipboard: ${text}`);
+//       alert(`Copied to clipboard: ${text}`);
+//     })
+//     .catch((error) => {
+//       console.error(`Could not copy email address: ${error}`);
+//     });
+// }
