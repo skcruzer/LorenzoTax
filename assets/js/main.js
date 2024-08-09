63a489495d092a188ce9d74d58e2fcacc477cdf6
Sean Cruz <seankennethcruz@gmail.com>
2024-08-08 20:16:36 -0700

    fixed footer to prevent x scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,3 @@
-/// When you click everywhere in the document
-$(document).click(function (event) {
-
-  /// If *navbar-collapse* is not among targets of event
-  if (!$(event.target).is('.navbar-collapse *')) {
-
-    /// Collapse every *navbar-collapse*
-    $('.navbar-collapse').collapse('hide');
-
-  }
-});
 // ===================== Copy Email Option =====================
 // function CopyMe(text) {
 //   navigator.clipboard.writeText(text)
